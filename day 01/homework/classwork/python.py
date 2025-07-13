8d2a33aba8b1e2f497a611631e352e9199910227
admin <admin@MacBook-Air-admin.local>
2025-07-13 14:40:36 +0400

    “განახლება”

---
+++ day 01/homework/classwork/python.py
@@ -0,0 +1,11 @@
+#davaleba mesame
+clothes = "pants tshirts"
+names = "niko saba gio"
+treetypes = "oak pine"
+resources = "water stone"
+
+#davaleba mexute
+grandfathers = "ramazi daviti"
+grandmothers = "maro cico"
+parents = "salome shotiko"
+siblings = "mari niko"
