c16aa1acf0a33169350a8034c2060eff32d9c22a
admin <admin@MacBook-Air-admin.local>
2025-07-26 19:17:56 +0400

    “განახლება”

---
+++ day 3/classwork/hgsd.py
@@ -0,0 +1,14 @@
+number = 50
+number2 = 42.2
+
+print(50+50)
+
+number1=15
+number3=46
+print(number1 + number3)
+
+print(65+34)
+print(64+76)
+
+print(34-24)
+print(54-13)
---
+++ day 3/homework/homework/fge.py
@@ -0,0 +1,54 @@
+#davaleba 2
+num1 = 34
+num2 = 46
+num3 = 53
+
+print(num1+num2+num3)
+
+#davaleba 3
+
+x = 32
+y =54
+print(32+54) 
+#an kide ese
+print(x+y)
+
+#davaleba 4
+length = 20
+width = 10
+print((length+width)*2)
+
+#davaleba 5
+#mimateba
+print(10+10)
+print(11+11)
+print(34+37)
+print(42+76)
+print(276+12)
+
+#gamokleba
+print(56-32)
+print(647-132)
+print(3864-2423)
+print(234-53)
+print(43-24)
+
+#gamravleba
+print(23*2)
+print(24*9)
+print(12*4)
+print(312*2)
+print(43*6)
+
+#gayopa
+print(54/3)
+print(23/4)
+print(54/2)
+print(72/3)
+print(21/4)
+
+print(23//2)
+print(45//3)
+print(12/5)
+print(34//4)
+print(67//3)
