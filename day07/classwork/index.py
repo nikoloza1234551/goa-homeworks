035bc17a3518fae13877178fe31288e38e036a16
admin <admin@MacBook-Air-admin.local>
2025-08-24 16:49:44 +0400

    second

---
+++ day07/classwork/index.py
@@ -0,0 +1,24 @@
+#input-ფუნქცია არის როდესაც ადამიანს შეაქვს ინფორმაცია
+#output-ფუნქცია არის როდესაც კომპიუტერს გამოაქვს ინფორმაცია ჩვენთვის
+
+#კონკატინაცია არის ორი ან მეტი სიტყვის ან სტრიქონის შეერთება
+
+num1="54"
+num2=2
+
+print((int)(num1)+num2)
+print((int)(num1)-num2)
+print((int)(num1)*num2)
+print((int)(num1)/num2)
+
+height=float(input("enter your height:"))
+print(type(height))
+
+
+
+
+#5.data convertion-არის ინფორმაციის გაცემა და მიღება
+
+#6.implicit-როდესაც ინფორმაცია ავტომატურად იცვლება
+#მაგ:output
+#explicitly-როდესაც ინფორმაციას ჩვენით ვცვლით
